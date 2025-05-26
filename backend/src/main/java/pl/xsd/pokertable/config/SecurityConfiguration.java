49b8dbfc4ce5349a7db7b61b56a17c6734efc522
Dekrate <mikdiakowski@interia.pl>
2025-05-26 15:24:11 +0200

    fix: cors security on deployed frontend

--- backend/src/main/java/pl/xsd/pokertable/config/SecurityConfiguration.java
+++ backend/src/main/java/pl/xsd/pokertable/config/SecurityConfiguration.java
@@ -59,7 +59,7 @@ public class SecurityConfiguration {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+		configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://dekrate.github.io/tsd-planning-poker/"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true);
